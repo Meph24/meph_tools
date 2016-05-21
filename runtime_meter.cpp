e68e0805924d47d213464f8b0d57f4d113ae76f0
HL65536 <HL65536@web.de>
2016-05-22 01:03:40 +0200

    Update runtime_meter.cpp

--- runtime_meter.cpp
+++ runtime_meter.cpp
@@ -3,7 +3,7 @@
 
 
 
-runtime_meter::runtime_meter(string ID, double precision, ostream* os = nullptr,bool longFormat = false):
+runtime_meter::runtime_meter(string ID, double precision, ostream* os, bool longFormat):
 ID(ID),
 inUse(false),
 os(os),
@@ -54,7 +54,7 @@ string runtime_meter::precisionToString(double prec)
 	{
 		if (prec == 1) return "s";
 		else if (prec == 1000) return "ms";
-		else if (prec == 1000000) return "µs";
+		else if (prec == 1000000) return "Âµs";
 		else if (prec == 1000000000) return "ns";
 		else if (prec == (1 / 60.0)) return "m";
 		else if (prec == (1 / 3600.0)) return "h";
